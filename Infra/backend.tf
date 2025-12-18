b11c8478a74e7f755caa9dad55dce2b2dc8a0cad
Janaki ram Padala <Padala.janakiram11@gmail.com>
2025-12-18 12:44:03 +0530

    Update backend.tf

--- Infra/backend.tf
+++ Infra/backend.tf
@@ -1,1 +1,9 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "devops-terraform-state"
+    key            = "github-oidc/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
